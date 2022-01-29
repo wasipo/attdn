d8a1a7a422b1d670e8d584e73f410c04bb434c17
horiguchi <horiguchiko@gmail.com>
2022-01-29 22:16:36 +0900

    細かいコンポーネント作成。

---
+++ packages/frontend/parts/attendance.tsx
@@ -0,0 +1,24 @@
+
+export const attendance = () => {
+    return (
+        <input type="text" />
+    )
+}
+
+export const clockingOut = () => {
+    return (
+        <input type="text" />
+    )
+}
+
+export const restTime = () => {
+    return (
+        <input type="text" />
+    )
+}
+
+export const resultTodayAttendance = () => {
+    return (
+        <input type="text" />
+    )
+}
---
+++ packages/frontend/parts/attendanceFunction.tsx
@@ -0,0 +1,2 @@
+
+export const addFunction = () => <button>追加情報</button>;
