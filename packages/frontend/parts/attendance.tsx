e97be1532f6e3d4f21e8c1d44ae5ec5d425a759f
horiguchi <horiguchiko@gmail.com>
2022-01-31 22:35:35 +0900

    TargetRowを各コンポーネントに送る

--- packages/frontend/parts/attendance.tsx
+++ packages/frontend/parts/attendance.tsx
@@ -1,24 +1,28 @@
 
-export const Attendance = () => {
+type workSchedule = {
+    rowNumber : number
+}
+
+export const Attendance = (props:workSchedule) => {
     return (
-        <input type="text" className="mt-1 py-2 px-3  focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md border-2" />
+        <input type="text" className="mt-1 py-2 px-3  focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md border" />
     )
 }
 
-export const ClockingOut = () => {
+export const ClockingOut = (props:workSchedule) => {
     return (
-        <input type="text" className="mt-1 py-2 px-3  focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md border-2" />
+        <input type="text" className="mt-1 py-2 px-3  focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md border" />
     )
 }
 
-export const RestTime = () => {
+export const RestTime = (props:workSchedule) => {
     return (
-        <input type="text" className="mt-1 py-2 px-3 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md border-2" />
+        <input type="text" className="mt-1 py-2 px-3 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md border" />
     )
 }
 
-export const ResultTodayAttendance = () => {
+export const ResultTodayAttendance = (props:workSchedule) => {
     return (
-        <input type="text" className="mt-1 py-2 px-3 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md border-2" />
+        <input type="text" className="mt-1 py-2 px-3 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md border" />
     )
 }
