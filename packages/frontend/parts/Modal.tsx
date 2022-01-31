1f8507ee1124ce4fd8e6ef7c0c7da235facd5ae3
horiguchi <horiguchiko@gmail.com>
2022-01-31 22:31:40 +0900

    Modal画面に必要項目追加

---
+++ packages/frontend/parts/Modal.tsx
@@ -0,0 +1,19 @@
+
+
+export const StartOverTime = () => {
+    return (
+        <div className="mt-6 block">
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="startOverTime">残業開始時間</label>
+            <input name="startOverTime" type="text" className="mt-1 py-2 px-3 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md border" />
+        </div>
+    )
+};
+
+export const EndOverTime = () => {
+    return (
+        <div className="mt-3 block">
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="endOverTime">残業終了時間</label>
+            <input name="endOverTime" type="text" className="mt-1 py-2 px-3 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md border" />
+        </div>
+    )
+};
---
+++ packages/frontend/parts/OverTime.tsx
@@ -0,0 +1,12 @@
+import {StartOverTime,EndOverTime} from '../parts/Modal';
+
+export const OverTime = () => {
+
+    return (
+        <div className="mt-2">
+            <StartOverTime />
+            <EndOverTime />
+        </div>
+    )
+
+}
