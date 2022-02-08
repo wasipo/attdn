a1caae024c802c99a7831c3bcd531357a30a8576
horiguchi <horiguchiko@gmail.com>
2022-02-09 08:56:08 +0900

    コミットもれ

--- packages/frontend/layouts/workScheduleLayout.tsx
+++ packages/frontend/layouts/workScheduleLayout.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { WorkScheduleRow, workScheduleRowState } from '../lib/store/WorkSchedule';
+// @ts-ignore
 import WorkSchedule from '../parts/WorkSchedule';
 
 
